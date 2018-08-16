c3fb446861adfaef780fc3e633def6ec80709f5b
chris <chrissantosproduction@gmail.com>
2018-08-16 07:29:03 -0500

    Found the problem

--- postinstall.js
+++ postinstall.js
@@ -4,18 +4,14 @@ const prettier = require('prettier');
 const path = require('path');
 
 const npm = spawn('npm', ['list', '-g', '--depth', '0']);
-console.log(
-    npm.output.forEach(out => {
-        out ? out.toString() : '';
-    })
-);
+
 let installPath = npm.output[1].toString();
-console.log(installPath);
-console.log(__dirname);
-console.log(fs.readdirSync(installPath));
 installPath = installPath.match(/^(\/.*)/g)[0];
 installPath = path.resolve(installPath);
 
+if (!fs.existsSync(installPath)) {
+    installPath = path.resolve('./');
+}
 console.log(installPath);
 
 const xpressoDir = path.join(installPath, 'node_modules', 'xpresso');
@@ -23,9 +19,6 @@ const config = {
     xpressoDir,
     templateDir: path.join(xpressoDir, 'templates')
 };
-
-console.log(config.xpressoDir);
-console.log(config.templateDir);
 
 if (!fs.existsSync(xpressoDir)) {
     fs.closeSync(fs.openSync(path.join(xpressoDir, 'config.json'), 'w'));
