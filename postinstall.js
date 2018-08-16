e7995565821d85625ee67b5aaf1fcf20a6bdd799
chris <chrissantosproduction@gmail.com>
2018-08-15 22:35:13 -0500

    Hmmmm

--- postinstall.js
+++ postinstall.js
@@ -5,8 +5,11 @@ const path = require('path');
 
 const npm = spawn('npm', ['list', '-g', '--depth', '0']);
 let installPath = npm.output[1].toString();
+console.log(installPath);
 installPath = installPath.match(/^(\/.*)/g)[0];
 installPath = path.resolve(installPath);
+
+console.log(installPath);
 
 const xpressoDir = path.join(installPath, 'node_modules', 'xpresso');
 const config = {
