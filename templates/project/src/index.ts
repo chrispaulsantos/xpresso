a86b2adaf9383db9f646eb63c359329f848b8921
chris <chrissantosproduction@gmail.com>
2018-11-11 00:16:08 -0600

    Added sendFile on catch all to allow serving of SPA's

--- templates/project/src/index.ts
+++ templates/project/src/index.ts
@@ -1,7 +1,9 @@
 import * as express from 'express';
 import { Request, Response } from 'express';
 import * as ws from 'express-ws';
+import * as fs from 'fs';
 import * as morgan from 'morgan';
+import * as path from 'path';
 import { connect } from './database';
 import { Server } from './server';
 import { Logger } from './services/logger';
@@ -37,6 +39,7 @@ connect(() => {
     // Create root app
     const wss = ws(express());
     const app = wss.app;
+    let isUILoaded: boolean = false;
 
     Server.initialize(app);
 
@@ -46,8 +49,18 @@ connect(() => {
     // STARTROUTES //
     // ENDROUTES //
 
+    // Public directory for loading a UI
+    const publicPath = path.join(__dirname, 'public');
+    app.use(express.static(publicPath, { index: false }));
     app.use('*', (req: Request, res: Response) => {
-        res.status(404).json({ message: 'RouteNotFound' });
+        const indexPath = path.join(publicPath, 'index.html');
+
+        if (!isUILoaded && !fs.existsSync(indexPath)) {
+            return res.status(404).send(`<h3>${req.url} not found</h3>`);
+        } else {
+            isUILoaded = true;
+            return res.sendFile(indexPath);
+        }
     });
 
     app.listen(process.env.PORT || 8080, () => {
