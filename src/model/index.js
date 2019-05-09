9bfc26ed2b45ae8377a3f1d479c5160a371dc5f3
chris <chrissantosproduction@gmail.com>
2019-05-08 23:39:22 -0500

    Checking if model exists

--- src/model/index.js
+++ src/model/index.js
@@ -6,11 +6,16 @@ const Replacement = require('./../replacement');
 const create = name => {
     const names = util.generateNames(name);
 
+    const modelPath = path.join(SRC_DIR, 'models', `${names.paramSingular}.ts`);
+    if (fs.existsSync(modelPath)) {
+        console.log('Model already exists');
+        process.exit(1);
+    }
+
     // Get the model template
     const template = fs.readFileSync(path.join(TEMPLATE_DIR, 'model')).toString();
 
     const replacements = [new Replacement('pascalSingular', names.pascalSingular)];
-    const modelPath = path.join(SRC_DIR, 'models', `${names.paramSingular}.ts`);
 
     console.log(`Creating Model:`, names.paramSingular);
     util.writeTemplate(template, modelPath, replacements);
